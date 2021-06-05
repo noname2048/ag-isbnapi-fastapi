69f5114f1ccd29298875d197b098bdc30f196baf
noname2048 <sungwook.csw@gmail.com>
2021-06-05 11:31:13 +0900

    refactor: main 로직 단축

--- main.py
+++ main.py
@@ -1,15 +1,8 @@
 from fastapi import FastAPI
-import requests
 from dotenv import load_dotenv
-import os
-import xml.etree.ElementTree as elemTree
-import xmltodict
-from PIL import Image
-from io import BytesIO
+
 from pymongo import MongoClient
-from pydantic import BaseModel
-from fastapi.responses import JSONResponse
-import json
+import aladin
 
 client = MongoClient("mongodb://root:example@mongo:27017")
 print(client.list_database_names())
@@ -26,36 +19,19 @@ async def root():
 @app.get("/isbn/{isbn}")
 async def isbn(isbn: str):
 
-    if (ret := books_collection.find_one({"isbn": isbn})) is not None:
+    ret = books_collection.find_one({"isbn": isbn})
+    if ret is not None:
         ret.pop("_id")
         return ret
 
-    r: requests.Response = requests.get(
-        "https://openapi.naver.com/v1/search/book_adv.xml",
-        headers={
-            (client_id := "X-Naver-Client-Id"): os.environ[client_id],
-            (client_secret := "X-Naver-Client-Secret"): os.environ[client_secret],
-        },
-        params={"d_isbn": isbn},
-    )
-
-    tree = elemTree.fromstring(r.text)
-    tree.find('channel/item')
-    data = dict()
-    for elem in list(tree.find('channel/item')):
-        data[elem.tag] = elem.text
-    data['isbn'] = data['isbn'].split(' ')[-1]
-    
-    img_link = data['image']
-    res = requests.get(img_link)
-    bytes_img = res.content
-    img = Image.open(BytesIO(bytes_img))
-    img.save(f"thumbnails/{isbn}.jpg")
+    try:
+        data = aladin.isbn_dict(isbn)
+    except aladin.AladinApiException as e:
+        return { "error": e.msg }
 
     books_collection.insert_one(data)
-
+    data.pop("_id")
     return data
-
 
 @app.get("/test")
 async def test():
