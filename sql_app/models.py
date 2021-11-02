6dc9489ebeeb9574ff792dd73744fd0158261ce3
noname2048 <sungwook.csw@gmail.com>
2021-11-02 23:31:41 +0900

    chore: change model table name

--- sql_app/models.py
+++ sql_app/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, DateTime
 from sqlalchemy.orm import relationship
 
 from .engine import Base
@@ -14,7 +14,8 @@ class User(Base):
 
 
 class BookRequest(Base):
+    __tablename__ = "bookrequests"
 
     id = Column(Integer, primary_key=True, index=True)
     isbn = Column(Integer, index=True)
-    date = Column()
+    date = Column(DateTime)
