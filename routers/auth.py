cfc627d67576b8fe697ec41d85ca350b7a66bbf1
noname2048 <sungwook.csw@gmail.com>
2021-11-22 23:22:23 +0900

    docs: add docs to google oauth2 login helper function

--- routers/auth.py
+++ routers/auth.py
@@ -14,8 +14,28 @@ router = APIRouter()
 secret_config = dotenv_values(Path(__file__).parents.parents / ".env")
 
 
-@router.post("/auth/google/login/redirected")
-async def signup(code: Optional[str] = Query(None)):
+@router.get("/auth/google/login/redirected", tags=["login", "oauth"])
+async def google_auth(code: Optional[str] = Query(None)):
+    """구글 로그인 OAuth 리다이렉트를 받는 함수
+    ## 1
+        프론트에서 구글 로그인 api, "https://accounts.google.com/o/oauth2/v2/auth" 로 안내
+        정보는 대략
+        clientId: "48231090303-eelp5r7prhev89t13ng9mt8f609pqqhg.apps.googleusercontent.com",
+        redirectUri: "http://localhost:8000/auth/api/v1/google/token",
+        responseType: "token",
+        scope: "https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile",
+        state: "letslogin",
+        loginHint: "sungwook.csw@gmail.com", // optional
+    ## 2
+        구글 로그인 api가 code를 들고 get으로 현재 함수 실행
+    ## 3
+        현재 함수에서 code를 access_token으로 바꾸는 작업 수행
+        # TODO: signup일 경우 refresh_token DB에 저장하기
+        # TODO: login, signup일 경우 access_token을 DB에 저장하기
+        # TODO: 요청을 수행할 때마다 access_token 갱신하기
+    ## 4
+        바꾼 access_token 을 json으로 리턴합니다.
+    """
     errors = {"error": "문제가 발생했습니다. code가 존재하지 않습니다."}
     if not code:
         return JSONResponse(
