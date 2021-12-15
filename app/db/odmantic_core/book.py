52c7130309d7dce86acfc136221f8bfed0ae22c4
noname2048 <sungwook.csw@gmail.com>
2021-12-16 06:10:25 +0900

    fix: fix typo and add created_at, updated_at

--- app/db/odmantic_core/book.py
+++ app/db/odmantic_core/book.py
@@ -6,12 +6,16 @@ from datetime import datetime as sys_datetime
 
 class Book(Model):
     title: str
-    desciption: Optional[str]
+    description: Optional[str]
     isbn13: int
     cover: str
     publisher: str
     price: int
     pub_date: sys_datetime
+    author: str
+
+    created_at: sys_datetime
+    updated_at: Optional[sys_datetime]
 
     class Config:
-        collection = "book"
+        collection = "books"
