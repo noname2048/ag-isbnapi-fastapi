619a292bd74f7fbcea28e0ce031d32d0b4fdc815
noname2048 <sungwook.csw@gmail.com>
2021-12-16 16:36:05 +0900

    feat: install pytest, add simple test for root(/)

---
+++ app/test_main.py
@@ -0,0 +1,12 @@
+from fastapi.testclient import TestClient
+
+from app.main import app
+
+client = TestClient(app)
+
+
+def test_read_main():
+    """main 함수를 읽고 루트(/) 응답상태코드가 200인지 확인"""
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"messege": "hello"}
