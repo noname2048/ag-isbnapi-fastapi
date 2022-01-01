aa63683fba0a2b22661e8fb7137a4962594fa901
noname2048 <sungwook.csw@gmail.com>
2022-01-01 19:37:50 +0900

    refact: refact mongodb connection

---
+++ app/nosql/__init__.py
@@ -0,0 +1,41 @@
+from app.settings.base import REPO_DIR
+from app.settings import config
+
+from motor.motor_asyncio import AsyncIOMotorClient
+from odmantic import AIOEngine
+
+
+try:
+    _mongo_db_name = config.get("MONGO_DB")
+    _mongo_db_password = config.get("MONGO_PASSWORD")
+    _mongo_db_user = config.get("MONGO_USER")
+except KeyError:
+    raise EnvironmentError("cannot find MONGO_DB from '.env' file")
+
+
+class _MongoDB:
+    def __init__(self):
+        self.client = None
+        self.engine = None
+        self.connect()
+
+    def connect(self):
+        if not self.client:
+            self.client = AsyncIOMotorClient(
+                f"mongodb+srv://{_mongo_db_name}:{_mongo_db_password}@cluster0.zywhp.mongodb.net/{_mongo_db_name}?retryWrites=true&w=majority"
+            )
+            self.engine = AIOEngine(
+                motor_client=self.client, database=f"{_mongo_db_name}"
+            )
+
+    def disconnect(self):
+        if self.client:
+            self.client.close()
+            self.client = None
+
+    def __del__(self):
+        self.disconnect()
+        super().__del__()
+
+
+mongo_db = _MongoDB()
---
+++ app/nosql/model.py
@@ -0,0 +1,44 @@
+from typing import Collection, Optional
+from odmantic import Model
+from datetime import datetime
+
+
+class TimeStampModel(Model):
+    created_at: datetime
+    updated_at: datetime
+
+
+class Book(TimeStampModel):
+    # id(str) will default
+    title: str
+    description: Optional[str]
+    isbn13: int
+    cover: str
+    publisher: str
+    price: int
+    pub_date: datetime
+    author: str
+
+    class Config:
+        collection = "books"
+
+
+class Request(Model):
+    # id(str) will default
+    isbn13: int
+    request_date: datetime
+    result_code: int
+    response_id: Optional[int]
+
+    class Config:
+        collection = "requests"
+
+
+class Response(Model):
+    # id(str) will default
+    isbn13: int
+    response_date: datetime
+    request_id: str
+
+    class Config:
+        Collection = "responses"
