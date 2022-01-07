448b32867cde13f0096fb20a86261944971e8512
noname2048 <sungwook.csw@gmail.com>
2022-01-07 20:47:54 +0900

    feat: add post method of requests

--- app/books.py
+++ app/books.py
@@ -1,8 +1,13 @@
-from typing import Optional
-from fastapi import APIRouter, Query, Path, HTTPException
+from typing import Optional, List
+from fastapi import APIRouter, Query, Path, HTTPException, Body, BackgroundTasks
 
 from app.nosql import mongo_db
 from app.nosql.model import Book, Request, Response
+import regex
+import datetime
+from app.task.aladin_api import do_request_task
+
+from pydantic import BaseModel
 
 router = APIRouter()
 
@@ -38,13 +43,50 @@ async def books_search(
     return ret
 
 
-@router.get("/requests", tags=["requests"])
+@router.get("/requests", tags=["requests"], response_model=List[Request])
 async def books_reqeusts_list(
     title: Optional[str] = Query(None),
     isbn13: Optional[int] = Query(None),
     limit: Optional[int] = Query(10, le=100),
 ):
-    pass
+    ret = await mongo_db.engine.find(Request, limit=limit)
+    return ret
+
+
+class ISBN13(BaseModel):
+    isbn13: int
+
+
+@router.post("/requests", tags=["requests"])
+async def books_requests_accept(
+    background_tasks: BackgroundTasks,
+    isbn13_list: List[ISBN13] = Body(...),
+):
+    request_list = []
+    for isbn13_obj in isbn13_list:
+        isbn13 = isbn13_obj.isbn13
+        if not regex.match(r"\d{13,13}", str(isbn13)):
+            n = datetime.datetime.now()
+            r = Request(isbn13=isbn13, request_date=n, result_code=400)
+            r = await mongo_db.engine.save(r)
+            request_list.append(r)
+        else:
+            target_book = await mongo_db.engine.find_one(
+                Book, Book.isbn13.match(isbn13)
+            )
+            if not target_book:
+                target_request = await mongo_db.engine.find_one(
+                    Request, Request.isbn13.match(isbn13)
+                )
+                if not target_request:
+                    n = datetime.datetime.now()
+                    r = Request(isbn13=isbn13, request_date=n, result_code=200)
+                    r = await mongo_db.engine.save(r)
+
+                    background_tasks.add_task(do_request_task, mongo_object_id=r.id)
+                    request_list.append(r)
+
+    return request_list
 
 
 @router.get("/reqeusts/{id}", tags=["requests"])
