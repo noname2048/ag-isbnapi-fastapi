2600dd213e9a6b74e9af774469d6e7672003ff57
noname2048 <sungwook.csw@gmail.com>
2022-01-12 02:55:41 +0900

    refact: refact main

--- app/main.py
+++ app/main.py
@@ -2,26 +2,15 @@ import uvicorn
 from fastapi import FastAPI, APIRouter
 from fastapi.middleware.cors import CORSMiddleware
 
-from app.routers.request import router as request_router
-
-# from app.routers.books import router as books_router
 from app.books import router as books_router
-from app.routers.search import router as search_router
-from app.routers.auth import router as auth_router
-from app.routers.oauth import router as oauth_router
-
 from app.nosql import connect_db, close_db
 
-# api
-# 요청하고 -> 나열하고 -> 검색하고
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from app.task.myscheduler import find_not_responded_request
+
 router = APIRouter(tags=["api/v1"], prefix="/api/v1")
 router.include_router(books_router, tags=["books"], prefix="/books")
-# router.include_router(request_router, tags=["request"], prefix="/request")
-# router.include_router(search_router, tags=["search"], prefix="/search")
-# router.include_router(books_router, tags=["books"], prefix="/books")
 
-# app
-app = FastAPI()
 
 origins = [
     "http://localhost:3000",
@@ -29,8 +18,7 @@ origins = [
     "http://localhost",
 ]
 
-all = ["*"]
-
+app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=all,
@@ -38,24 +26,15 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 app.include_router(router)
 app.add_event_handler("startup", connect_db)
 app.add_event_handler("shutdown", close_db)
 
-# async def get_mongodb_client() -> AsyncIOMotorClient:
-#     pass
+app.scheduler = AsyncIOScheduler()
+app.scheduler.add_job(find_not_responded_request, trigger="cron", minute="*/5")
+app.scheduler.start()
 
 
-# async def connect_mongodb():
-#     pass
-
-
-# async def disconnect_mongodb():
-#     pass
-
-
-# main
 @app.get("/")
 async def hello():
     return {"messege": "hello"}
