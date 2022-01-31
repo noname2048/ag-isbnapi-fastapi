9ba63e61d3d4abf933e8148cbf850f8f937caef6
noname2048 <sungwook.csw@gmail.com>
2022-01-31 23:47:42 +0900

    feat: improve sql base features

--- app/database/conn.py
+++ app/database/conn.py
@@ -1,6 +1,7 @@
 import logging
 from fastapi import FastAPI
 from sqlalchemy import create_engine
+import sqlalchemy
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
@@ -21,4 +22,42 @@ class SQLAlchemy:
             databse_url,
             echo=echo,
             pool_recycle=pool_recycle,
+            pool_pre_ping=True,
         )
+
+        self._session = sessionmaker(
+            autocommit=False, autoflush=False, bind=self._engine
+        )
+
+        @app.on_event("startup")
+        def startup():
+            self._engine.connect()
+            logging.info("DB connected.")
+
+        @app.on_event("shutdown")
+        def shutdown():
+            self._session.close_all()
+            self._engine.dispose()
+            logging.info("DB disconnected")
+
+        def get_db(self):
+            if self._session is None:
+                raise Exception("must be called 'init_app'")
+            db_session = None
+            try:
+                db_session = self._session()
+                yield db_session
+            finally:
+                db_session.close()
+
+        @property
+        def session(self):
+            return self.get_db
+
+        @property
+        def engine(self):
+            return self._engine
+
+
+db = SQLAlchemy()
+Base = declarative_base()
