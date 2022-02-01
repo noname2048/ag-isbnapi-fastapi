1e1504b66ecb4933f30291723b3fc62f45bd16cc
noname2048 <sungwook.csw@gmail.com>
2022-02-02 02:07:58 +0900

    feat: add auth logic of register

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,5 +1,19 @@
 from typing import Optional
-from fastapi import APIRouter, Query
+from datetime import datetime, timedelta
+
+import bcrypt
+import jwt
+from fastapi import APIRouter, Depends
+
+from sqlalchemy.orm import Session
+from starlette.responses import JSONResponse
+
+from app import models
+from app.common.consts import JWT_SECRET, JWT_ALGORITHM
+from app.database.conn import db
+from app.database.schema import Users
+from app.models import SnsType, Token, UserToken
+
 
 router = APIRouter(
     prefix="/api/v1/auth",
@@ -11,7 +25,50 @@ router = APIRouter(
         404: {"description": "Not found"},
     },
 )
-import bcrypt
+
+
+async def is_email_exist(email: str) -> bool:
+    get_email = Users.get(email=email)
+    if get_email:
+        return True
+    return False
+
+
+async def create_access_token(*, data: dict = None, expires_delta: int = 1):
+    to_encode = data.copy()
+    if expires_delta:
+        to_encode.update({"exp": datetime.utcnow() + timedelta(hours=expires_delta)})
+    encoded_jwt = jwt.encode(to_encode, JWT_SECRET, algorithm=JWT_ALGORITHM)
+    return encoded_jwt
+
+
+@router.post("/register/{sns_type}", status_code=200, response_model=Token)
+async def register(
+    sns_type: SnsType,
+    reg_info: models.UserRegister,
+    session: Session = Depends(db.session),
+):
+    if sns_type == SnsType.email:
+        is_exist = await is_email_exist(reg_info.email)
+        if not reg_info.email or reg_info.pw:
+            return JSONResponse(
+                status_code=400, content=dict(msg="Email and PW must be provided")
+            )
+        if is_exist:
+            return JSONResponse(status_code=400, content=dict(msg="EMAIL_EXISTS"))
+
+        hash_pw = bcrypt.hashpw(
+            reg_info.pw.encode("utf-8"), bcrypt.gensalt()
+        )  ## TODO: gensalt not saved. is ok?
+        new_user = Users.create(
+            session, auto_commit=True, pw=hash_pw, email=reg_info.email
+        )
+        token = dict(
+            Authorization=f"Bearer {create_access_token(data=UserToken.from_orm(new_user).dict(exclude={'pw', 'marketing_agree'}))}"
+        )
+        return token
+
+    return JSONResponse(status_code=400, content=dict(msg="NOT_SUPPORTED"))
 
 
 @router.post("/signup", tags=["auth", "signup"])
