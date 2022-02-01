91654b5bd6adc9cd2a738965388739d35f8f1e4e
noname2048 <sungwook.csw@gmail.com>
2022-02-02 02:07:18 +0900

    feat: add consts

---
+++ app/models.py
@@ -0,0 +1,33 @@
+from enum import Enum
+
+from pydantic import BaseModel
+from pydantic.networks import EmailStr
+
+
+class UserRegister(BaseModel):
+    email: EmailStr
+    pw: str = None
+
+
+class SnsType(str, Enum):
+    email: str = "email"
+    facebook: str = "facebook"
+    google: str = "google"
+    kakao: str = "kakao"
+
+
+class Token(BaseModel):
+    Authorization: str = None
+
+
+class UserToken(BaseModel):
+    id: int
+    pw: str = None
+    email: str = None
+    name: str = None
+    phone_number: str = None
+    profile_img: str = None
+    sns_type: str = None
+
+    class Config:
+        orm_mode = True
