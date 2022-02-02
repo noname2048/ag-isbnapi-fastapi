d71dab14b3027757deb7bcc176f503faef1504e4
noname2048 <sungwook.csw@gmail.com>
2022-02-02 22:49:07 +0900

    feature: add post login logic with sns types

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,3 +1,4 @@
+from lib2to3.pgen2 import token
 from typing import Optional
 from datetime import datetime, timedelta
 
@@ -27,14 +28,14 @@ router = APIRouter(
 )
 
 
-async def is_email_exist(email: str) -> bool:
+def is_email_exist(email: str) -> bool:
     get_email = Users.get(email=email)
     if get_email:
         return True
     return False
 
 
-async def create_access_token(*, data: dict = None, expires_delta: int = 1):
+def create_access_token(*, data: dict = None, expires_delta: int = 1) -> str:
     to_encode = data.copy()
     if expires_delta:
         to_encode.update({"exp": datetime.utcnow() + timedelta(hours=expires_delta)})
@@ -68,6 +69,29 @@ async def register(
         )
         return token
 
+    return JSONResponse(status_code=400, content=dict(msg="NOT_SUPPORTED"))
+
+
+@router.post("/login/{sns_type}", status_code=200)
+async def login(sns_type: SnsType, user_info: models.UserRegister):
+    if sns_type == SnsType.email:
+        is_exist = is_email_exist(user_info.email)
+        if not user_info.email or not user_info.pw:
+            return JSONResponse(
+                status_code=400, content=dict(msg="Email and PW must be provided")
+            )
+        if not is_exist:
+            return JSONResponse(status_code=400, content=dict(msg="NO_MATCH_USER"))
+        user = Users.get(email=user_info.email)
+        is_verified = bcrypt.checkpw(
+            user_info.pw.encode("utf-8"), user.pw.encode("utf-8")
+        )
+        if not is_verified:
+            return JSONResponse(status_code=400, content=dict(msg="NO_MATCH_USER"))
+        token = dict(
+            Authorization=f"Bearer {create_access_token(data=UserToken.from_orm(user).dict(exclude={'pw', 'marketing_agree'}))}"
+        )
+        return token
     return JSONResponse(status_code=400, content=dict(msg="NOT_SUPPORTED"))
 
 
