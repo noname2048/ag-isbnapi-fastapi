934a20de4732eb7ea1684e465a406e1bc924a4bf
noname2048 <sungwook.csw@gmail.com>
2022-02-08 23:55:19 +0900

    feat: add env file settings but missing

--- app/common/config.py
+++ app/common/config.py
@@ -1,11 +1,22 @@
+"""
+config.py
+
+서버를 구동하는데 있어 필요한 설정값은 모두 해당 파일에 기재
+민감한 정보는 .env에 따로 적되, 여기서 불러와서 저장
+
+현 파일에는 dataclass를 이용하는 '방법1'과 (차후 attrs로 업데이트)
+pydantic의 BaseSettings를 이용하는 '방법2'가 혼재
+"""
 from typing import List, Union
 from os import environ
 from pathlib import Path
 from dataclasses import dataclass, asdict
 from functools import lru_cache
 
-from pydantic import BaseSettings
+from pydantic import BaseSettings, Field
 from dotenv import dotenv_values
+
+from app.common.consts import EXCEPT_PATH_LIST
 
 config_dir = Path(__file__).resolve().parent
 base_dir = config_dir.parent
@@ -84,17 +95,37 @@ def get_mongodb_url(*, secrets: dict) -> str:
 
 
 class Settings(BaseSettings):
+    """
+    pydantic의 문서를 참조하여 작성된 fastapi 기본 설정
+
+    나중에 다시 봐도 딱 알 수 있도록 상세히 기재
+    소문자로 바꿔야 하는지 고민 중
+    """
+
     BASE_DIR: Path = base_dir
     ALLOWED_ORIGIN: list = [
         "http://localhost:3000",
         "http://localhost:8000",
         "http://localhost",
     ]
-    POSTGRESQL_URL: str = get_postgresql_url(secrets=secrets)
+    AUTHORIZATION_EXCEPT_ENDPOINT_LIST: list = [
+        "/",
+        "/health",
+        "/openapi.json",
+    ]
+    AUTHORIZATION_EXCEPT_ENDPOINT_REGEX: str = "^(/docs|/redoc|/auth)"
+    # DSN은 Data Source Name을 뜻함
+    POSTGRESQL_DSN: str = get_postgresql_url(secrets=secrets)
     POSTGRESQL_ECHO: bool = True
-    MONGODB_URL: str = get_mongodb_url(secrets=secrets)
+    MONGODB_DSN: str = get_mongodb_url(secrets=secrets)
+    # JWT_SECRET은 공개가 어려움
+    JWT_SECRET: str = Field(..., env="BASE_JWT_SECRET")
+    JWT_ALGORITHM: str = Field(..., env="BASE_JWT_ALGORITHM")
 
 
 @lru_cache()
 def get_settings():
     return Settings()
+
+
+settings = get_settings()
