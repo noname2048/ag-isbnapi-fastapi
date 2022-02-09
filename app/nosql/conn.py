b9045aa06fbf797fbd20c450da93c9eafa826352
noname2048 <sungwook.csw@gmail.com>
2022-02-10 02:18:02 +0900

    feat: update monogodb connector

---
+++ app/nosql/conn.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+from motor.motor_asyncio import AsyncIOMotorClient
+
+
+class MongoDB:
+    def __init__(self):
+        self._client: AsyncIOMotorClient = None
+
+    def init_motor(self, app: FastAPI, dsn: str):
+        self.dsn = dsn
+
+        @app.on_event("startup")
+        def startup():
+            self._client = AsyncIOMotorClient(self.dsn)
+
+        @app.on_event("shutdown")
+        def shutdown():
+            self._client.close()
+
+    @property
+    def client(self):
+        if not self._client:
+            raise Exception("use mongodb before init")
+
+        return self._client
--- app/nosql/manager.py
+++ app/nosql/manager.py
@@ -6,28 +6,34 @@ from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorDatabase
 
 
 class MongoManager:
-    client: AsyncIOMotorClient = None
-    db: AsyncIOMotorDatabase = None
+    def __init__(self):
+        self._client: AsyncIOMotorClient = None
+        self._db: AsyncIOMotorDatabase = None
 
-    def __init__(self, app: FastAPI, **kwargs):
-        self.path = kwargs.get("path")
-        if app is not None:
+    def init_motor(self, app: FastAPI, dsn: str):
+        self.dsn = dsn
 
-            @app.on_event("startup")
-            def startup():
-                self.connect_db(self.path)
+        @app.on_event("startup")
+        def startup():
+            logging.info("Connecting to MongoDB")
+            self.client = AsyncIOMotorClient(self.dsn, maxPoolSize=10, minPoolSize=10)
+            self.db = self.client.main_db
+            logging.info("Connected to MongoDB")
+            self.connect_db(self.path)
 
-            @app.on_event("shutdown")
-            def shutdown():
-                self.close_db()
+        @app.on_event("shutdown")
+        def shutdown():
+            logging.info("Closing connection with MongoDB")
+            self.client.close()
+            logging.info("Closed connection with MongoDB")
 
-    async def connect_db(self, path: str):
-        logging.info("Connecting to MongoDB")
-        self.client = AsyncIOMotorClient(path, maxPoolSize=10, minPoolSize=10)
-        self.db = self.client.main_db
-        logging.info("Connected to MongoDB")
+    def get_db(self):
 
-    async def close_db(self):
-        logging.info("Closing connection with MongoDB")
-        self.client.close()
-        logging.info("Closed connection with MongoDB")
+        try:
+            yield self.client
+        finally:
+            self._client.close()
+
+    @property
+    def session(self) -> function:
+        return self.get_db
