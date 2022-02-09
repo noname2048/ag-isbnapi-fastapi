a53f048f4ac49332cafee7bac8f0d54aabb7109f
noname2048 <sungwook.csw@gmail.com>
2022-02-09 23:46:55 +0900

    feat: temporary disable middlewares

--- app/main.py
+++ app/main.py
@@ -7,12 +7,11 @@ from fastapi import FastAPI, APIRouter
 from fastapi.security import APIKeyHeader
 from fastapi.middleware.cors import CORSMiddleware
 
-from app.common.config import get_config
+from app.common.config import get_config, settings
 from app.middlewares.token_validator import AccessControl
 from app.middlewares.trusted_host import TrustedHostMiddleware
 from app.database.conn import db
 from app.routes import index  # , books, auth, users
-
 
 API_KEY_HEADER = APIKeyHeader(name="Authorization", auto_error=False)
 
@@ -20,34 +19,32 @@ API_KEY_HEADER = APIKeyHeader(name="Authorization", auto_error=False)
 def create_app() -> FastAPI:
     """fastapi 앱을 만드는 함수
 
-    :return: FastAPI app
+    :return: 설정된 FastAPI app
     """
     # config
-    c = get_config()
-    conf_dict = asdict(c)
     # app
     app = FastAPI()
     # DB
     # db.init_app(app, **conf_dict)
     # redis
     # middleware
-    app.add_middleware(
-        AccessControl,
-        except_path_list=[],
-        except_path_regex="",
-    )
+    # app.add_middleware(
+    #     AccessControl,
+    #     except_path_list=settings.AUTHORIZATION_EXCEPT_ENDPOINT_LIST,
+    #     except_path_regex=settings.AUTHORIZATION_EXCEPT_ENDPOINT_REGEX,
+    # )
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=c.ALLOWED_ORIGIN,
+        allow_origins=settings.ALLOWED_ORIGIN,
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
     )
-    app.add_middleware(
-        TrustedHostMiddleware,
-        allowed_hosts=["*"],
-        except_path=["/health"],
-    )
+    # app.add_middleware(
+    #     TrustedHostMiddleware,
+    #     allowed_hosts=["*"],
+    #     except_path=settings.AUTHORIZATION_EXCEPT_ENDPOINT_LIST,
+    # )
     # routes
     app.include_router(index.router)
     # app.include_router(books.router, tags=["books", "api"], prefix="/api/v1/books")
