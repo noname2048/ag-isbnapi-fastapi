96e5ce55206f49f38608cc580d4a52085c917588
noname2048 <sungwook.csw@gmail.com>
2022-02-10 23:31:02 +0900

    refact: refact config.py

--- app/common/config.py
+++ app/common/config.py
@@ -4,8 +4,9 @@ config.py
 서버를 구동하는데 있어 필요한 설정값은 모두 해당 파일에 기재
 민감한 정보는 .env에 따로 적되, 여기서 불러와서 저장
 
-현 파일에는 dataclass를 이용하는 '방법1'과 (차후 attrs로 업데이트)
-pydantic의 BaseSettings를 이용하는 '방법2'가 혼재
+현 파일에는 dataclass를 이용하다가 list[str]을 사용할 수 없어
+attrs 패키지로 업그레이드 하려다가, 
+pydantic의 BaseSettings를 이용하는 '방법2'를 채택
 """
 from typing import List, Union
 from os import environ
@@ -20,78 +21,6 @@ from app.common.consts import EXCEPT_PATH_LIST
 
 config_dir = Path(__file__).resolve().parent
 base_dir = config_dir.parent
-secrets = dotenv_values(config_dir / ".env")
-
-
-@dataclass
-class Config:
-    """기본적인 Config"""
-
-    BASE_DIR = base_dir
-
-
-@dataclass
-class LocalConfig(Config):
-    ALLOWED_ORIGIN = [
-        "http://localhost:3000",
-        "http://localhost:8000",
-        "http://localhost",
-    ]  # QUESTION: if type noticed, error occurs
-    POSTGRES_URL: str = (
-        f"postgresql+psycopg2://{secrets['POSTGRESQL_USER']}:{secrets['POSTGRESQL_PW']}"
-        f"@{'localhost'}:{'5432'}/{secrets['POSTGRESQL_DBNAME']}"
-    )
-    POSTGRES_ECHO: bool = True
-    MONGODB_URL: str = ()
-    TRUSTED_HOSTS = ["*"]
-
-
-@dataclass
-class ProdConfig(Config):
-    pass
-
-
-@lru_cache()
-def get_config() -> Union[LocalConfig, ProdConfig]:
-    """
-    API_ENV 설정에 따라 설정값을 가져옵니다. (default=local)
-
-    :return: LocalConfig | ProdConfig
-    """
-    config = dict(prod=ProdConfig(), local=LocalConfig())
-    return config[environ.get("API_ENV", "local")]
-
-
-def get_postgresql_url(*, secrets: dict) -> str:
-    """
-    시크릿으로 부터 키값을 추출하여 db url을 리턴합니다.
-
-    :return: db_url
-    """
-    POSTGRESQL_USER = secrets["POSTGRESQL_USER"]
-    POSTGRESQL_PW = secrets["POSTGRESQL_PW"]
-    HOST = "localhost"
-    PORT = "8000"
-    POSTGRESQL_DBNAME = secrets["POSTGRESQL_DBNAME"]
-
-    return (
-        f"postgresql+psycopy2:"
-        f"//{POSTGRESQL_USER}:{POSTGRESQL_PW}"
-        f"@{HOST}:{PORT}/{POSTGRESQL_DBNAME}"
-    )
-
-
-def get_mongodb_url(*, secrets: dict) -> str:
-    MONGODB_USER = secrets["MONGODB_USER"]
-    MONGODB_PW = secrets["MONGODB_PW"]
-    MONGODB_DBNAME = secrets["MONGODB_DBNAME"]
-
-    return (
-        f"mongodb+srv:"
-        f"//{MONGODB_USER}:{MONGODB_PW}"
-        f"@cluster0.zywhp.mongodb.net/{MONGODB_DBNAME}"
-        f"?retryWrites=true&w=majority"
-    )
 
 
 class Settings(BaseSettings):
@@ -99,28 +28,29 @@ class Settings(BaseSettings):
     pydantic의 문서를 참조하여 작성된 fastapi 기본 설정
 
     나중에 다시 봐도 딱 알 수 있도록 상세히 기재
-    소문자로 바꿔야 하는지 고민 중
     """
 
-    BASE_DIR: Path = base_dir
-    ALLOWED_ORIGIN: list = [
+    base_dir: Path = base_dir
+    allowed_origin: list = [
         "http://localhost:3000",
         "http://localhost:8000",
         "http://localhost",
     ]
-    AUTHORIZATION_EXCEPT_ENDPOINT_LIST: list = [
+    authorization_except_list: list = [
         "/",
         "/health",
         "/openapi.json",
     ]
-    AUTHORIZATION_EXCEPT_ENDPOINT_REGEX: str = "^(/docs|/redoc|/auth)"
-    # DSN은 Data Source Name을 뜻함
-    POSTGRESQL_DSN: str = get_postgresql_url(secrets=secrets)
-    POSTGRESQL_ECHO: bool = True
-    MONGODB_DSN: str = get_mongodb_url(secrets=secrets)
-    # JWT_SECRET은 공개가 어려움
-    JWT_SECRET: str = Field(..., env="BASE_JWT_SECRET")
-    JWT_ALGORITHM: str = Field(..., env="BASE_JWT_ALGORITHM")
+    authorization_except_regex: str = "^(/docs|/redoc|/auth)"
+    postgresql_dsn: str = "postgresql+psycopy2://user:password@localhost:5432/test"
+    postgresql_echo: bool = True
+    mongodb_dsn: str = "mongodb+srv://user:password@localhost:27017/test"
+    jwt_secret: str = "4321dcba"
+    jwt_algorithm: str = "HS256"
+
+    class Config:
+        env_file = config_dir / ".env"
+        env_file_encoding = "utf-8"
 
 
 @lru_cache()
