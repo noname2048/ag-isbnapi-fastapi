aa85a832613f32b61a9fabc0d88d7c6bbc9673d0
noname2048 <sungwook.csw@gmail.com>
2022-03-02 23:25:57 +0900

    feat: add single crawl endpoint

--- app/routes/crawl.py
+++ app/routes/crawl.py
@@ -3,11 +3,13 @@ from fastapi import APIRouter, Query, BackgroundTasks
 from app.nosql.conn import mongodb
 import datetime
 
+from app.task.back_crawl import single_crawl
+
 router = APIRouter()
 
 
 @router.get("/crawl")
-async def smple_crwal_request(
+async def simple_crwal_request(
     bg_tasks: BackgroundTasks,
     isbn: str = Query(..., title="isbn", regex=r"^[0-9]{13}$"),
 ):
@@ -28,3 +30,20 @@ async def smple_crwal_request(
     result = await collection.find_one(inserted.inserted_id)
     result["_id"] = str(result["_id"])
     return result
+
+
+@router.get("/cral/back")
+async def single_back_crawl(isbn: str = Query(..., regex=r"^[0-9]{13}$")):
+    requests_coll = mongodb.client["isbn"]["requests"]
+    request = await requests_coll.find_one({"isbn": isbn})
+
+    if not request:
+        return {"msg": "request not exists, call '/crawl' first"}
+
+    books_coll = mongodb.client["isbn"]["books"]
+    book = await books_coll.find_one({"isbn": isbn})
+    if book:
+        return book
+
+    new_book = await single_crawl(request["_id"])
+    return new_book
