9822e055246165f05129401ac89330b0682d5bb1
noname2048 <sungwook.csw@gmail.com>
2022-03-02 23:18:53 +0900

    refactor: refact back_crawl logic

--- app/task/back_crawl.py
+++ app/task/back_crawl.py
@@ -4,7 +4,7 @@ import requests
 import json
 from enum import Enum
 import datetime
-from app.task.image_upload import upload
+from app.task.image_upload import download_and_upload, is_in_s3, upload
 import asyncio
 
 
@@ -28,22 +28,27 @@ aladin_api_url = "http://www.aladin.co.kr/ttb/api/ItemLookUp.aspx"
 ttb_key = settings.aladin_ttbkey
 
 
-async def single_crawl(id: str):
+async def single_crawl(requests_id: str):
+    # requests 콜렉션의 docuemnt id 가져오기
     collection = mongodb.client["isbn"]["requests"]
     request = await collection.find_one({"_id": id})
     if not request:
         raise Exception("something wrong")
+    isbn = request["isbn"]
 
+    # 해당 isbn을 가지고 알라딘에 api 요청
     res = requests.post(
         aladin_api_url,
         {
             "TTBKey": ttb_key,
             "itemIdType": "isbn13",
-            "ItemId": str(request["isbn"]),
+            "ItemId": isbn,
             "Cover": "Big",
             "output": "js",
         },
     )
+
+    # text를 json으로 변환
     try:
         text_result = res.text[:-1]
         json_result = json.loads(text_result, strict=False)
@@ -55,6 +60,7 @@ async def single_crawl(id: str):
     if not item:
         raise Exception("something wrong3")
 
+    # 변환된 json에서 데이터 추출
     useful_data = {
         "title": item[0]["title"],
         "description": item[0]["description"],
@@ -63,11 +69,16 @@ async def single_crawl(id: str):
         "price": item[0]["priceStandard"],
         "pub_date": datetime.datetime.strptime(item[0]["pubDate"], "%Y-%m-%d"),
         "author": item[0]["author"],
+        "cover": f"{request['isbn']}.jpg",
     }
-    # 이미 있으면 안올리는 로직 추가하기
-    await upload(request["isbn"], item[0]["cover"])
 
-    useful_data["cover"] = f"{request['isbn']}.jpg"
+    # 체크하고 표지 다운받아 업로드
+    if is_in_s3(item[0]["isbn13"]) == False:
+        download_and_upload(item[0]["isbn13"], item[0]["cover"])
 
+    # 몽고디비에 books 콜렉션에 문서 저장
     book_coll = mongodb.client["isbn"]["books"]
-    await book_coll.insert_one(useful_data)
+    inserted = await book_coll.insert_one(useful_data)
+    new_book = await book_coll.find_one({"_id": inserted.inserted_id})
+
+    return new_book
--- app/task/image_upload.py
+++ app/task/image_upload.py
@@ -38,7 +38,26 @@ async def upload(isbn13: int, img_url: HttpUrl):
             return True
 
 
-async def upload_when_not_exists(isbn13: str, img_url: HttpUrl) -> bool:
+async def is_in_s3(isbn13: str) -> bool:
+    global s3, bucket
+
+    isbn_pattern = re.compile(r"^\d{13}$")
+    if not isbn_pattern.match(isbn13):
+        raise Exception("none valid isbn13")
+
+    obj = bucket.Object(f"{isbn13}.jpg")
+    try:
+        obj.load()
+    except ClientError as e:
+        if e.response["Error"]["Code"] == 404:
+            # is in s3
+            return True
+
+    # not in s3
+    return False
+
+
+async def download_and_upload(isbn13: str, img_url: HttpUrl) -> bool:
     global s3, bucket
 
     # isbn 형식확인
@@ -51,14 +70,8 @@ async def upload_when_not_exists(isbn13: str, img_url: HttpUrl) -> bool:
     if response.status_code != 200:
         raise Exception("img_url not valid")
 
-    # 파일이 이미 존재하는지 확인
+    # 파일 업로드
     obj = bucket.Object(f"{isbn13}.jpg")
-    try:
-        obj.load()
-    except ClientError as e:
-        if e.response["Error"]["Code"] == 404:
-            raise Exception("file already exists")
-        raise Exception("unknwon Error")
-
     obj.upload_fileobj(response.raw)
+
     return True
