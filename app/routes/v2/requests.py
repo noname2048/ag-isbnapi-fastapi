44a0f96b3997a147cff5519df4c425c6c533b9c2
noname2048 <sungwook.csw@gmail.com>
2022-03-06 19:44:20 +0900

    feat: make post reqeusts, v2

--- app/routes/v2/requests.py
+++ app/routes/v2/requests.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter
+from datetime import datetime
+from fastapi import APIRouter, Body
 from app.odmantic.connect import singleton_mongodb
 from app.odmantic.models import Request
 
@@ -6,10 +7,31 @@ router = APIRouter()
 
 
 @router.get("/requests")
-async def requests():
+async def list_requests():
     """"""
     engine = singleton_mongodb.engine()
     requests = await engine.find(Request, {}, limit=100)
     if requests:
         return requests
     return []
+
+
+@router.post("/requests")
+async def make_request(
+    isbn: str = Body(..., regex=r"^\d{13}$"), update: bool = Body(False)
+):
+    """"""
+    engine = singleton_mongodb.engine()
+    request = await engine.find(Request, {"isbn": isbn})
+    if request and update:
+        request.status = "need update"
+        await engine.save(request)
+        return request
+
+    if request:
+        raise Exception("already requested")
+
+    now = datetime.utcnow()
+    new_request = Request(isbn=isbn, created_at=now, updated_at=now, status="requested")
+    request = await engine.save(Request)
+    return request
