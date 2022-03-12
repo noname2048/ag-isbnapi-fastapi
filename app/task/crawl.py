dbb1023f55ab6339a4c79d8c103994eac2625d33
noname2048 <sungwook.csw@gmail.com>
2022-03-12 15:10:54 +0900

    feat: complete crawl v2

--- app/task/crawl.py
+++ app/task/crawl.py
@@ -2,8 +2,12 @@ import requests
 import re
 import json
 from datetime import datetime
+
+from boto3 import resource
+
 from app.odmantic.connect import singleton_mongodb
 from app.odmantic.models import Request, Book
+from app.common.config import settings
 
 
 aladin_api_url = ""
@@ -12,6 +16,7 @@ isbn13_pattern = re.compile(r"^\d{13}$")
 
 
 async def f1(mongo_object_id: str):
+    # parameter를 바탕으로 request 불러오기
     engine = singleton_mongodb.engine
     request = await engine.find_one(Request, Request.id == mongo_object_id)
     if not request:
@@ -21,6 +26,7 @@ async def f1(mongo_object_id: str):
     if not isbn13_pattern.match(isbn13):
         raise Exception()
 
+    # 알라딘에 isbn13으로 요청
     response = requests.post(
         aladin_api_url,
         {
@@ -31,7 +37,10 @@ async def f1(mongo_object_id: str):
             "output": "js",
         },
     )
+    if response.status_code != 200:
+        raise Exception("알라딘에서 200이 아닌 응답을 수신")
 
+    # 반환받은 text를 json으로 변경
     try:
         text = response.text[:-1]
         json_data = json.loads(text, strict=False)
@@ -46,26 +55,33 @@ async def f1(mongo_object_id: str):
         await engine.save(request)
         raise Exception()
 
-    book_data = {
-        "title": item[0]["title"],
-        "description": item[0]["description"],
-        "isbn13": item[0]["isbn13"],
-        "publisher": item[0]["publisher"],
-        "price": item[0]["priceStandard"],
-        "pub_date": datetime.datetime.strptime(item[0]["pubDate"], "%Y-%m-%d"),
-        "author": item[0]["author"],
-        "cover": f"{request['isbn']}.jpg",
-    }
+    # 변환된 json에서 필요한 book 데이터 추출
+    img_url = item[0]["cover"]
+    new_book = Book(
+        title=item[0]["title"],
+        description=item[0]["description"],
+        isbn13=item[0]["isbn13"],
+        publisher=item[0]["publisher"],
+        price=item[0]["priceStandard"],
+        pub_date=datetime.datetime.strptime(item[0]["pubDate"], "%Y-%m-%d"),
+        author=item[0]["author"],
+        cover=f"{isbn13}.jpg",
+    )
 
-    # check aws
-    # if not, upload
+    # cover 이미지가 aws에 있는지 확인
+    bucket = resource(
+        "s3",
+        region_name="ap-northeast-2",
+        aws_access_key_id=settings.boto3_aws_access_id,
+        aws_secret_access_key=settings.boto3_aws_access_key,
+    ).Bucket("job-book-image")
+    obj = bucket.Object(new_book.cover)
 
-    Book(validate=False)
-    Book.title
-    Book.description
-    Book.isbn13
-    Book.pub_date
-    Book.publisher
-    Book.price
-    Book.author
-    Book.cover
+    try:
+        obj.load()
+    except Exception as e:
+        if e.response["Error"]["Code"] == 404:
+            img_res = requests.get(img_url)
+            obj.upload_fileobj(img_res.raw)
+
+    saved_book = await engine.save(new_book)
