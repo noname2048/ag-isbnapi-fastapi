3316b0c900a8c891e02bf1788ce92566083ab298
noname2048 <sungwook.csw@gmail.com>
2022-03-17 23:38:42 +0900

    resarch: something cause in JSON.stringify

--- app/routes/v2/test.py
+++ app/routes/v2/test.py
@@ -3,16 +3,18 @@ from fastapi import APIRouter, Body
 # import logging
 # from app.main import mylogger
 from app.utils.logger import mylogger
+import logging
 
 router = APIRouter()
 
 
 @router.get("/test/echo")
 async def echo_test(isbn: str = Body("")):
-    mylogger.warning(isbn)
     return {"isbn": isbn}
 
 
 @router.post("/test/echo")
 async def echo_text(isbn: str = Body("")):
-    return {"isbn": isbn}
+    isbn: str = isbn.rstrip('"')
+    mylogger.info(f"+{isbn}+")
+    return {"isbn": int(isbn)}
