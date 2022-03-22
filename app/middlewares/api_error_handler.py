165038e0f190b9a3c1fe5da018504ee1ff7db887
noname2048 <sungwook.csw@gmail.com>
2022-03-22 23:11:00 +0900

    feat: add api_exception handler

---
+++ app/middlewares/api_error_handler.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI, middleware, Request, Response
+import time
+
+from app.exceptions import APIException
+
+
+async def api_erroer_handler(request: Request, call_next):
+    """API에러가 레이즈될경우 처리하는 미들웨어"""
+    try:
+        response = await call_next(request)
+    except APIException as api_exception:
+        api_exception.detail
+        api_exception.msg
+        api_error = {"detail": api_exception.detail, "msg": api_exception.msg}
+        return Response({"error": api_error})
+
+    return response
