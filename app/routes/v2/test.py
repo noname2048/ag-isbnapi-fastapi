8e7249639c42c0477c7b7c84620b68e9fe7f04aa
noname2048 <sungwook.csw@gmail.com>
2022-04-01 22:30:22 +0900

    feat: add test endpoint for delete db

--- app/routes/v2/test.py
+++ app/routes/v2/test.py
@@ -1,9 +1,11 @@
 from typing import Optional
-from fastapi import APIRouter, Body
+from fastapi import APIRouter, Body, BackgroundTasks
 
 # import logging
 # from app.main import mylogger
 from app.utils.logger import mylogger
+from app.odmantic.connect import singleton_mongodb
+from app.odmantic.models import Request, Book
 import logging
 from pydantic import BaseModel
 
@@ -24,3 +26,21 @@ class Request(BaseModel):
 async def echo_text(isbn: Request):
     mylogger.info(f"+{isbn}+")
     return {"isbn": isbn}
+
+
+@router.get("/test/db/clean")
+async def clean_db(background_task: BackgroundTasks):
+    background_task.add_task(delete_all)
+
+    return {"msg": "start clean"}
+
+
+async def delete_all():
+    engine = singleton_mongodb.engine
+    requests = await engine.find(Request, {})
+    books = await engine.find(Book, {})
+    for request in requests:
+        await engine.delete(request)
+    for book in books:
+        await engine.delete(book)
+    mylogger.warn("delete done.")
