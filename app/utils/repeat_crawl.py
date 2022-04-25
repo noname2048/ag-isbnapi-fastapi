643519eeff6ced068d1ab9bf82e4071b7ed47c04
noname2048 <sungwook.csw@gmail.com>
2022-04-25 19:42:44 +0900

    feat: change repeat work to new respond func

--- app/utils/repeat_crawl.py
+++ app/utils/repeat_crawl.py
@@ -1,33 +1,31 @@
 from fastapi import FastAPI
 from fastapi_utils.tasks import repeat_every
 
-from app.odmantic.connect import singleton_mongodb
-from app.odmantic.models import Request
+from app.odmantic import get_engine
+from app.odmantic.models import Request, RequestForm
 from app.task.crawl import f2
 from app.utils.logger import mylogger
 
+from app.task.v2.response import respond_single_request
+
 
 def init_repeat_crawl(app: FastAPI):
+    """register starup event"""
+
     @app.on_event("startup")
-    @repeat_every(seconds=5 * 60)
-    async def repeat_crawl() -> None:
-        mylogger.info("start repeat crawl!")
-        engine = singleton_mongodb.engine
-        requests = await engine.find(
-            Request,
-            Request.status.in_(["requested", "need update"]),
-            limit=100,
+    @repeat_every(seconds=60)
+    async def bulk_respond():
+        mylogger.info("checking db...")
+        engine = get_engine()
+        rf_list = await engine.find(
+            RequestForm,
+            RequestForm.response_date == None,
+            limit=20,
         )
-
-        if not requests:
-            mylogger.info("no requests find! - crawl end")
-
+        if not rf_list:
+            mylogger.info("checking done")
         else:
-            id_list = [request.id for request in requests]
-            id_len = len(id_list)
-            mylogger.warn(f"start crawl {id_len}")
-
-            books = await f2(id_list)
-
-            book_len = len(books)
-            mylogger.info(f" repeat crawl end!")
+            num = len(rf_list)
+            for idx, rf in enumerate(rf_list):
+                await respond_single_request(rf.id)
+                mylogger.info(f"{ rf.isbn } ({idx}/{num})")
